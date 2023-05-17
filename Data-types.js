5e6a00a7a11a6c1e74464c53704cf60a37422410
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-17 09:19:30 -0700

    Ch 1. Storing Values with the Assignment Operator

---
+++ Data-types.js
@@ -0,0 +1,21 @@
+/* Data types:
+undefined, null, Boolean, Symbol, Number, and Object 
+*/
+
+/*
+ var myName = "Beau"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14 */
+
+var a; 
+var b = 2;
+console.log(a);
+a = 7;
+
+b = a; 
+
+console.log(a)
