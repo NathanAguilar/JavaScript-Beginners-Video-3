8fddf947055b035728124e34f78044a8e8dc44b8
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-17 12:20:00 -0700

    Ch 5 Math Operations

---
+++ math-operations.js
@@ -0,0 +1,33 @@
+// compound assignment with augmented addition
+var a = 3;
+var b = 17;
+
+    // this is a faster way of doing it
+a +=12; // instead of a = a + 12 
+b +=9; // instead of b = 9 + b
+
+
+
+    // this is a faster way of doing it
+// compound assignment with augmented substraction
+var a = 11;
+var b = 9;
+
+
+a-= 6; // instead of a = a - 6
+b-= 15; //instead of b = b - 15
+
+// compound assignment with augmented multiplication
+var a = 5;
+var b = 12
+
+a*= 5; //instead of a = a * 5;
+b*= 3  //insetead of b = 3 * b;
+
+
+// compound assignment with augmented division
+var a = 48;
+var b =108
+
+a/= 12;  // instead of a = a / 12;
+b/= 4;   // instead of b = b / 4;
