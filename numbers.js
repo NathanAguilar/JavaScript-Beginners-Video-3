901c4d9dafad979aeceec49ce05b6a3534d3b77c
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-17 11:08:18 -0700

    Ch 4 Numbers

---
+++ numbers.js
@@ -0,0 +1,38 @@
+// adding numbers
+var sum = 10 + 10;
+console.log(sum);
+
+// substracting umbers
+var diffreence = 45 - 3;
+
+// multiplying numbers
+var product = 8 * 10;
+
+// diving numbers
+var quotient = 66/33;
+
+// incrementing numbers
+var myVar = 87;
+
+myVar++; // just a ++ to add 1(shortcut)
+
+// decrementing numbers
+var myVar = 11;
+
+myVar--;
+
+// decimal numbers
+var ourDecimal = 5.7;
+
+var myDecimal = 0.009
+
+// multiply decimals
+var product = 2.0 * 2.5;
+console.log(product);
+
+// divide decimals
+var quotient = 4.4 /2.0
+
+// finding a remainder
+var remainder;
+remainder = 11 % 3;
