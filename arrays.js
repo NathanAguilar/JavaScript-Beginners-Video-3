01cac7196991f84596363b7bc3718ec48126467c
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-17 20:09:15 -0700

    Ch 8. Arrays

---
+++ arrays.js
@@ -0,0 +1,122 @@
+// example
+/* var ourArray = ["John", 23];
+
+var myArray = ["Quincy", 1]; */
+
+
+
+// NESTED ARRAYS
+// example
+/* var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = [["bulls", 23], ["wite sox"]] */
+
+// ACCESS ARRAY DATA WITH  INDEXES
+// example 
+/* var ourArray = [50,60,70]
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var ourArray = [50,60,70];
+
+var myData = myArray[0];
+console.log(myData) */
+
+
+
+
+// MODIFY ARRAY DATA WITH INDEXES
+// example
+/* var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99] */
+
+// Setup
+/* var ourArray = [18,64,99];
+
+myArray[0] = 45
+console.log(myArray) */
+
+
+
+
+// ACCESS MULTI-DIMENSIONAL ARRAYS WITH INDEXES
+// setup
+/* var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
+console.log(myData);
+ */
+
+
+
+
+
+// MANIPULATE AARAYS WITH PUSH()
+/* var ourArray = ["Stimpson", "J", "Cat"];
+ourArray.push(["happy", "joy"])
+// our Array now equals ["Stimpson", "J", "Cat", ["happy", "joy"]]
+
+// setup
+var myArray = [["John", 23], ["cat", 2]];
+
+myArray.push(["dog", 3]) */
+
+
+
+
+
+// MANIPULATE ARRAYS WITH POP()
+/* var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();a
+// removedFromOurArray nowequals 3, and ourArray now equals [1,2]
+
+// setup
+var myArray = [["John", 23], ["cat", 2]];
+
+var removedFromOurArray = myArray.pop();
+
+console.log(myArray) */
+
+
+
+
+
+
+
+// MANIPULATE ARRAYS WITH SHIFT()
+/* var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["car"]];
+
+// setup
+
+var myArray = [["John", 23], ["dog", 3]];
+
+var removedFromOurArray = myArray.shift(); */
+
+
+
+
+
+
+
+// MANIPULATE ARRAYS WITH UNSHIFT()
+/* var ourArray = ["Stimpson", "J", ["cat"]];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+myArray.unshift(["Paul", 35]) */
+
+
+
+
+
+
+// SHOPPING LIST
+var myList =[["cereal", 3], ["milk", 2], ["bananas", 3], ["juice", 2], ["eggs", 12]]; 
+
