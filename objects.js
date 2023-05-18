ea47b59a90a53ec7d9110c45052ae78cac93c634
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-18 14:30:46 -0700

    Ch 12. Objects

---
+++ objects.js
@@ -0,0 +1,75 @@
+// BUILD JAVASCRIPT OBJECTS
+/* var ourDog = {
+    "name": "Quicy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+}; */
+
+
+
+
+
+
+
+// ACCESSING OBJECT PROPERTIRS WITH DOT NOTATION
+/* var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+}
+
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt; */
+
+
+
+
+
+
+
+// ACCESSING OBJECT PROPERTIES WITH BRACKET NOTATION
+/* var testObj = {
+    'an entree': 'hamburger',
+    'my side': 'veggies'
+}
+
+var entreeValue = testObj['an entree']
+var drinkValue = testObj['the drink'] */
+
+
+
+
+
+
+
+
+
+
+// ACCESSING OBJECT PROPERTIES WITH VARIABLES
+/* var testObj = {
+    12: 'Namath',
+    16: 'Montana'
+}
+var playerNumber = 16;
+var player = testObj[playerNumber]; */
+
+
+
+
+
+
+
+
+// UPDATING OBJECT PROPERTIES & DELETING
+var myDog = {
+    'name': 'Happy Coder',
+    'legs': 4,
+    'tails': 1,
+    'friends': ['freeCodeCamp Campers']
+};
+
+myDog['bark'] = 'woof!'
+
+// to delete 
+delete myDog.tails;
+
