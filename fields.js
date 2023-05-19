ee9302a8007ddce192fa7859977b4e53566f3aef
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-19 15:37:07 -0700

    Ch 18. Fields

---
+++ fields.js
@@ -0,0 +1,3 @@
+// WRITE CONCISE OBJECT LITERAL DECLARATIONS USING SIMPLE FIELDS
+const createPerson = (name, age, gender) => ( {name, age, gender });
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
