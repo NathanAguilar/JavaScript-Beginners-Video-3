983e656339186feb716cee9283e360d48b0053a7
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-18 17:47:54 -0700

    Ch 14. Fractions

---
+++ fractions.js
@@ -0,0 +1,87 @@
+// GENERATE RANDOM FRACTIONS
+/* function randomFraction() {
+
+    return Math.random();
+}
+
+console.log(randomFraction()); */
+
+
+
+
+
+// GENERATE RANDOM WHOLE NUMBERS
+/* var randomNumberBewteen0and19 = Math.floor(Math.random() * 10)
+
+function randomWholeNum() {
+
+    return Math.floow(Math.random() * 10);
+}
+
+console.log(randomWholeNum()); */
+
+
+
+
+
+// GENERATE RANDOM WHOLE NUMBERS WITHIN A RANGE
+/* function randomRange(myMin, myMax) {
+
+    return Math.floor(Math.random() * (myMax = myMin + 1)) + myMin;
+
+}
+
+var myRandom = randomRange(5, 15);
+
+console.log(myRandom); */
+
+
+
+
+
+// USE THE PARSEINT FUNCTION
+/* function convertToInteger(str) {
+    return parseInt(str);
+}
+
+convertToInteger("56"); */
+
+
+
+
+
+// USE THE PARSEINT FUNCTION WITH A RADIX
+/* function convertToInteger(str) {
+    return parseInt(str, 2)
+}
+
+convertToInteger("10011"); */
+
+
+
+
+
+
+//  USE THE CONDITIONAL(TERNARY) OPERATOR
+/* function checkEqual(a, b) {
+    return a == b ? true : false;
+
+    return a === b;
+}
+
+checkEqual(1, 2); */
+
+
+
+
+// USE MUTIPLE CONIDTIONAL (TERNARY) OPERATORS
+function checkSign(num) {
+    return num > 0 ? "positive" : num < 0 ? "negative" : "zero"
+}
+
+console.log(checkSign(0)); 
+
+
+
+
+
